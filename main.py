2324ea6d0867993a2e1a8d0a84533e657998614b
PawelWal <pawel.walkowiak@hotmail.com>
2022-06-01 17:34:30 +0200

    repair pylint corectness

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+'''Basic hello world'''
 
 def print_hi(name):
+    '''Takes in name and print hello'''
     print(f'Hello {name}')
 
 
-# Extra line
-# Extra line
 if __name__ == '__main__':
     print_hi('World')
-
-
