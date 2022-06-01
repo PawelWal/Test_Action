c551242d6bec7b8500ab4706c0b70e7fa99060e8
PawelWal <pawel.walkowiak@hotmail.com>
2022-06-01 17:43:41 +0200

    add new feature

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ def print_hi(name):
     print(f'Hello {name}')
 
 
+def print_bye(name):
+    '''Takes in name and print bye'''
+    print(f'Goodbye {name}')
+
+
 if __name__ == '__main__':
     print_hi('World')
+    print_bye('World')
